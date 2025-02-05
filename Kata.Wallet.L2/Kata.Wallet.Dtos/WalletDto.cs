4d0daa7730d6f59adada9bb0539f3b3ae168ce8c
Andres Mellimaci <amellimaci@atio.com.mx>
2025-02-05 10:24:12 -0300

    Added validations to WalletDto

--- Kata.Wallet.L2/Kata.Wallet.Dtos/WalletDto.cs
+++ Kata.Wallet.L2/Kata.Wallet.Dtos/WalletDto.cs
@@ -1,12 +1,21 @@
 ﻿using Kata.Wallet.Domain;
+using System.ComponentModel.DataAnnotations;
 
 namespace Kata.Wallet.Dtos;
 
 public class WalletDto
 {
     public int Id { get; set; }
+
+    [Range(0, double.MaxValue, ErrorMessage = "The balance cannot be negative.")]
     public decimal Balance { get; set; }
+
+    [StringLength(20, ErrorMessage = "The document cannot be longer than 20 characters.")]
     public string? UserDocument { get; set; }
+
+    [StringLength(50, ErrorMessage = "Username cannot be longer than 50 characters..")]
     public string? UserName { get; set; }
+
+    [Required(ErrorMessage = "Currency is required.")]
     public Currency Currency { get; set; }
 }
