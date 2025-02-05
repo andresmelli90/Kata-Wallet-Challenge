bd8c1c0d19e9589f65a7ffd5a2fa2ded79d69588
Andres Mellimaci <amellimaci@atio.com.mx>
2025-02-05 10:13:54 -0300

    Added Serilog

--- Kata.Wallet.L2/Kata.Wallet.Api/Program.cs
+++ Kata.Wallet.L2/Kata.Wallet.Api/Program.cs
@@ -2,11 +2,21 @@ using Kata.Wallet.Database;
 using Kata.Wallet.Services.Interfaces;
 using System.Text.Json.Serialization;
 using Kata.Wallet.Services.Services;
+using Serilog;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddDbContext<DataContext>();
+
+// Configure logs
+Log.Logger = new LoggerConfiguration()
+    .WriteTo.Console()
+    .WriteTo.File("Logs/app-log-.txt", rollingInterval: RollingInterval.Day)
+    .Enrich.FromLogContext()
+    .CreateLogger();
+
+builder.Host.UseSerilog();
 
 // Register services
 builder.Services.AddScoped<IWalletService, WalletService>();
@@ -24,6 +34,8 @@ builder.Services.AddSwaggerGen();
 builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
 
 var app = builder.Build();
+
+app.UseSerilogRequestLogging();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
